75650732d932f2a78558bfda573a6ecff5257b33
Fred Faeger <fred@tawakemono.com>
2022-12-12 15:33:03 +0900

    Changed the selector for the property

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,8 +30,8 @@ def scan_source(list_url)
   result_properties = page.css('div.result_box')
   result_properties.each do |property|
     property = {
-      title: property.css('a').text.split('　')[0],
-      link: 'https://www.city.ueda.nagano.jp/' + property.css('a')[0].attributes["href"].value
+      # title: property.css('a').text.split('　')[0],
+      link_id: property.css('a')[0].attributes["href"].value.scan(/\d+/).first
     }
     properties.push(property)
   end
@@ -40,5 +40,5 @@ end
 
 # end
 
-list_url = "https://www.city.ueda.nagano.jp/akiyabank/search.php?hid_flg_search=1&page=1&k_syozaichi%5B%5D=1&k_syozaichi%5B%5D=2&k_syozaichi%5B%5D=3&k_syozaichi%5B%5D=4&k_syozaichi%5B%5D=5&k_syozaichi%5B%5D=0"
+list_url = "https://www.city.ueda.nagano.jp/akiyabank/search.php?hid_flg_search=1&&k_syozaichi%5B%5D=1&k_syozaichi%5B%5D=2&k_syozaichi%5B%5D=3&k_syozaichi%5B%5D=4&k_syozaichi%5B%5D=5&k_syozaichi%5B%5D=0&page=1"
 scan_source(list_url)
